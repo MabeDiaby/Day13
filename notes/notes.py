c161ea157331dc8e32f2bd1e7365c34fa506dd57
MabeDiaby <diabymabe@gmail.com>
2022-12-02 15:33:54 -0500

    changed 20 to 21

--- notes/notes.py
+++ notes/notes.py
@@ -1,11 +1,11 @@
 # ############DEBUGGING#####################
-
-# # Describe Problem
-# def my_function():
-#   for i in range(1, 20):
-#     if i == 20:
-#       print("You got it")
-# my_function()
+import random
+# Describe Problem
+def my_function():
+  for i in range(1, 21): #Change from 20 to 21
+    if i == 20:
+      print("You got it")
+my_function()
 
 # # Reproduce the Bug
 # from random import randint
